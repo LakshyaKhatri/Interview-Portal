d01967dc1cf0a81aed2b696dad568a46cf91b60a
Lakshya Khatri <lakshyak67@gmail.com>
2021-01-06 22:55:55 +0530

    Fix Issue: Undefined Status

--- dashboard/api/serializers.py
+++ dashboard/api/serializers.py
@@ -3,6 +3,14 @@ from dashboard.models import Application
 
 
 class ApplicationSerializer(serializers.ModelSerializer):
+    type = serializers.CharField(source='get_type_display')
+    technology = serializers.StringRelatedField()
+    current_status = serializers.StringRelatedField()
+
     class Meta:
         model = Application
-        fields = '__all__'
+        fields = [
+            'id', 'name', 'email', 'type',
+            'technology', 'current_status',
+            'applied_on', 'moved_by', 'updated_on',
+        ]
