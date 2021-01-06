e1a5c36550fffa0b48fff5ca795d537cb0852c52
Lakshya Khatri <lakshyak67@gmail.com>
2021-01-06 11:47:58 +0530

    Update Dashboard to require login

--- interview_portal/views.py
+++ interview_portal/views.py
@@ -1,4 +1,8 @@
 from allauth.account.views import LoginView
+from django.views import generic
+from .models import Application
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 
 class CustomLoginView(LoginView):
@@ -6,3 +10,10 @@ class CustomLoginView(LoginView):
     Changes the default login template from Django-Allauth
     """
     template_name = 'interview_portal/login.html'
+
+
+@method_decorator(login_required, name='dispatch')
+class ApplicationListView(generic.ListView):
+    model = Application
+    template_name = 'interview_portal/index.html'
+    context_object_name = 'applications'
