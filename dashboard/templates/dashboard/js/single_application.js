b746fd0acb59fa9260b6f8c2ee652fc3f7efc609
Lakshya Khatri <lakshyak67@gmail.com>
2021-01-11 12:20:18 +0530

    Add ajax to send status update data

---
+++ dashboard/templates/dashboard/js/single_application.js
@@ -0,0 +1,28 @@
+let current_status = $('#status-select').val();
+
+$('#status-select').on('change', function() {
+  current_status = this.value;
+});
+
+const updateButton = $('.update-btn');
+
+updateButton.on('click', function() {
+  $('.spinner').show();
+  updateButton.prop("disabled",true);
+
+  $.post(
+      '../api/v1/update-status/' + $('#current-application').text() + '/',
+      $('#update-status-form').serialize()).done(function(response){
+        const updateTextSpan = updateButton.children('span');
+
+        setTimeout(function (){
+          updateTextSpan.text("Updated");
+          $('.spinner').hide();
+          setTimeout(function (){
+            updateTextSpan.text("Update");
+            updateButton.prop("disabled",false);
+          }, 1000);
+        }, 1000);
+      }
+    );
+});
